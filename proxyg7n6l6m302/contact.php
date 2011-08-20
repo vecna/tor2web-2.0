74e275c5cb08a4073b52161c6bdc4226329f2ca2
Some Random GlobaLeaks contributors <team@globaleaks.org>
2011-08-20 23:05:46 +0200

    Update with vecnas modifications

--- proxyg7n6l6m302/contact.php
+++ proxyg7n6l6m302/contact.php
@@ -9,33 +9,8 @@ else {
 ?>
 <html>
 <head>
-   <title>Contact the tor2web node maintainer</title>
-   <style type="text/css">
-html,body {
-      text-align: center;
-      border-top: 0.2em solid #ce6c1c;
-      font: 1em Verdana, Arial, Tahoma;
-      color: black;
-      /* background: #0b1933; */
-      background: #f5f5f5;
-      position: absolute;
-      top:0; left:0;
-      height: 250px;
-      z-index: 100000;
-      width: 100%;   margin: 0; padding: 0;
-}
-      #wrap {
-         width: 500px;
-         margin: 0 auto;
-      }
-      
-      table {
-      } 
-     
-      table input {
-         width: 220px;
-      } 
-   </style>
+<title>Contact the tor2web node maintainer</title>
+<link rel="stylesheet" type="text/css" href="tor2web.css">
 </head>
 
 <body>
@@ -59,12 +34,13 @@ if(isset($_POST['send'])) {
 
 else {
 ?>
-<h1>You are informing us of a <?php echo $type;?></h1>
-<p>You are going to file a <b><?php echo $type;?></b> complaint with Tor2web. Tor2web is not hosting any kind of content but just acts as a proxy to Tor Hidden Services. Even if we block access to that content via the Tor2web proxy the content will be still accessible by using a Tor client. We know that Tor2web may be used also to publish illegal content. Tor2web does not endorse illegal content and is willing to block any illegal material that could be internet-exposed through Tor2web. However, to be again very clear, even if we block it through tor2web, the content will still be accessible via the .onion URL from a Tor enabled browser</p>
+<div class="tor2web-title">You are informing us of a <?php echo $type;?></div>
+
+<p class="tor2web-text">You are going to file a <b><?php echo $type;?></b> complaint with Tor2web. Tor2web is not hosting any kind of content but just acts as a proxy to Tor Hidden Services. Even if we block access to that content via the Tor2web proxy the content will be still accessible by using a Tor client. We know that Tor2web may be used also to publish illegal content. Tor2web does not endorse illegal content and is willing to block any illegal material that could be internet-exposed through Tor2web. However, to be again very clear, even if we block it through tor2web, the content will still be accessible via the .onion URL from a Tor enabled browser</p>
 <form action="" method="POST">
 <table>
-<tr><td>Tor2web URL:</td><td><input type="text" name="url"/></td></tr>
-<tr><td>Contact of reporter (email):</td><td><input type="text" name="email"/></td></tr>
+<tr><td>Tor2web URL:</td><td><input type="text" class="tor2web-textarea" name="url"/></td></tr>
+<tr><td>Contact of reporter (email):</td><td><input type="text" class="tor2web-textarea" name="email"/></td></tr>
 <tr><td>Motivation of complaint:</td><td><textarea cols="30" rows="10" name="content"></textarea></td></tr>
 <tr><td><input type="submit" name="send"/></td></tr>
 </table>
